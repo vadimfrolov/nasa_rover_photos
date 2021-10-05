417f359a210d5a3f8963524631f83dc96babedf2
Vadim Frolov <vadimf74@ya.ru>
2021-10-06 00:05:17 +0300

    added get button and changed component name

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="The Mars Files" />
+    <MarsPhotos msg="The Mars Files" />
+    <button class="search-button">Get Photos</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MarsPhotos from "./components/MarsPhotos.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MarsPhotos,
   },
 };
 </script>
@@ -24,5 +25,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.search-button {
+  background-color: #105bd8;
+  border-radius: 5px;
+  border-width: 0;
+  color: #fff;
+  cursor: pointer;
+  font-size: 1.7rem;
+  display: inline-block;
+  font-weight: 700;
+  line-height: 1.5;
+  padding: 1rem 2rem;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: nowrap;
+  touch-action: manipulation;
 }
 </style>
